a873f7c21fa3a17c88d774a427818149497ae5c4
Toan Quach <toanqc@gmail.com>
2015-09-12 02:13:26 -0500

    [PO-0001] Day 1: setup project and home page
    - Add css lib / js lib
    - Add home page
    - Image / css  and so on

---
+++ src/main/webapp/assets/js/controller/app.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var app = angular.module('portfolio', [ 'ui.router',
+		'ngCookies', 'pascalprecht.translate', 'ngMessages' ]);
+
+app.config(function($translateProvider, $translatePartialLoaderProvider) {
+
+	// Initialize angular-translate
+	$translateProvider.useLoader('$translatePartialLoader', {
+		urlTemplate : 'i18n/{part}_{lang}.json'
+	});
+	$translateProvider.preferredLanguage('en');
+	$translateProvider.useSanitizeValueStrategy(null);
+	$translateProvider.useLocalStorage();
+	$translatePartialLoaderProvider.addPart('global');
+	$translatePartialLoaderProvider.addPart('header-footer');
+
+});
+
+app.controller('appCtrl', function($scope, $translate) {
+	$scope.changeLanguage = function(langKey) {
+		$translate.use(langKey);
+	};
+});
+
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.when('', '/');
+	$urlRouterProvider.otherwise('404');
+
+	$stateProvider.state('/', {
+		url : '/',
+		templateUrl : 'views/home.html'
+	}).state('accounts-register', {
+		url : '/accounts/register',
+		templateUrl : 'views/account-register.html',
+		controller : 'accountCtrl'
+	}).state('accounts-login', {
+		url : '/accounts/login',
+		templateUrl : 'views/login.html',
+		controller : 'loginCtrl'
+	}).state('accounts-home', {
+		url : '/accounts/home',
+		templateUrl : 'views/account-home.html',
+		access : {
+			requiredLogin : true
+		}
+	}).state('accounts-success', {
+		url : '/accounts/success',
+		templateUrl : 'views/account-success.html'
+	}).state('404', {
+		url : '/404',
+		templateUrl : 'views/errors/404.html'
+	});
+});
+
+app.run(function($cookies, $rootScope, $state) {
+
+	$rootScope.$on('$stateChangeStart', function(event, toState, toParams) {
+		var isAuthenticationRequired = toState.access
+				&& toState.access.requiredLogin;
+
+		var currentUser = $cookies.get('currentUser');
+		if (currentUser) {
+			$rootScope.currentUser = currentUser;
+		}
+
+		if (isAuthenticationRequired && currentUser === undefined) {
+			event.preventDefault();
+			$state.go('accounts-login');
+		}
+	});
+});
