33826c459da253984d6d719dbcfc180d0693a69d
Toan Quach <toanqc@gmail.com>
2015-09-13 14:47:31 -0500

    [PO-0002] Day 3: add who-i-am page & contact page
    - add some stuff about who-i-am page
    - add

--- src/main/webapp/assets/js/controller/main.js
+++ src/main/webapp/assets/js/controller/main.js
@@ -3,4 +3,6 @@
 app.controller('mainCtrl', function($translate, $translatePartialLoader) {
 	$translatePartialLoader.addPart('home');
 	$translate.refresh();
+
+	$.getScript("assets/js/portfolio.js");
 });
